4445d7e1ffddc4e771402492dad0f1b8a4982888
Ferran00 <ferran.iglesias.barenys@estudiantat.upc.edu>
2021-11-11 02:55:12 +0100

    scene changes to test stuff removed interactable attribute from CarPart

--- Assets/Scripts/CarPart.cs
+++ Assets/Scripts/CarPart.cs
@@ -8,9 +8,6 @@ public class CarPart : MonoBehaviour
     //the higher the better
     //maybe we'll remove this from the mother class
     [SerializeField] public uint rarity=0;
-
-    //temporary attribute. we'll probably disable the car part in a more efficient way in the future.
-    private bool interactable = true;
     
     // Start is called before the first frame update
     void Start()
@@ -25,23 +22,18 @@ public class CarPart : MonoBehaviour
     }
     public void addedToInventory()
     {
-        if (interactable)
-        {
-            //it should also stop existing as a physical object, since its physics are still being calculated.
-            //it may not be a problem since we may remove all physics from it (Fortnite style)
-            //setActive hasn't worked for me because it disappears completely and therefore can't be re-enabled
-
-            MeshRenderer mr = gameObject.GetComponent<MeshRenderer>();
-            mr.enabled = false;
-            //stops being able to be clicked.
-            interactable = false;
-        }
+        //it should stop being able to be clicked.
+        //it should also stop existing as a physical object, since its physics are still being calculated.
+        //it may not be a problem since we may remove all physics from it (Fortnite style)
+        //setActive hasn't worked for me because it disappears completely and therefore can't be re-enabled
+        
+        MeshRenderer mr = gameObject.GetComponent<MeshRenderer>();
+        mr.enabled = false;
     }
 
     public void outFromInventory()
     {
         MeshRenderer mr = gameObject.GetComponent<MeshRenderer>();
         mr.enabled = true;
-        interactable = true;
     }
 }
