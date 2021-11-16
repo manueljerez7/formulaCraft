02507ef552aeb5ad3fca31d6f941f531b8a27bf5
Ferran00 <ferran.iglesias.barenys@estudiantat.upc.edu>
2021-11-16 23:54:06 +0100

    Car part spawn randomness done.
    created a scene "RandomnessTest" to test it

--- Assets/Scripts/CarPartSpawnPoint.cs
+++ Assets/Scripts/CarPartSpawnPoint.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class CarPartSpawnPoint : MonoBehaviour
 {
-    [SerializeField] uint rarity;
+    //rarity of the spawn point. Must be either 0 or 1.
+    [FormerlySerializedAs("rarity")] [SerializeField] uint spawnPointRarity;
 
     public GameObject[] partPrefabs;
     // Start is called before the first frame update
     void Start()
     {
         //instantiates a carPart of random type
-        GameObject carPart = Instantiate(partPrefabs[Random.Range(0, 3)], transform.position, Quaternion.identity);
-        carPart.SendMessage("setRarity", rarity);
+        int random_part_num = Random.Range(0, 3);
+        print("random_part_num = " + random_part_num);
+        GameObject carPart = Instantiate(partPrefabs[random_part_num], transform.position, Quaternion.identity);
+        
+        //choose the rarity of the part, randomly chosen between 0 and 1 or between 1 and 2 depending on the rarity of the spawn point
+        //based on a 30%-70% chance
+        //(read GDD for more info)
+        uint partRarity;
+        if (Random.Range(0, 10) > 2)
+            partRarity = spawnPointRarity;
+        else
+            partRarity = spawnPointRarity + 1;
+        
+        carPart.SendMessage("setRarity", partRarity);
     }
 
     // Update is called once per frame
