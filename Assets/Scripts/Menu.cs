9d5f7bde3b3698a3b4d2a836fb2e98aaadcc4678
manueljerez7 <manu.jerez2000@gmail.com>
2021-11-23 21:25:08 +0100

    TextField added on the menu for leaderboard

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,11 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+	public string name;
+	public GameObject inputField;
+
     public void PlayGame(){
+		name = inputField.GetComponent<Text>().text;
+		if(name=="") {
+      		print("Null name");
+			name= "Unnamed";
+ 		}
+		PlayerPrefs.SetString("username",name);
         SceneManager.LoadScene("LootingArea");
+		
     }
     
     public void QuitGame()
