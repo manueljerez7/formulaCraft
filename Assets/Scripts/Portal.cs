543e2621dccf23f3e7c2c53010dcd45097503590
Ferran00 <ferran.iglesias.barenys@estudiantat.upc.edu>
2021-12-16 14:49:55 +0100

    lower volume on lootingArea SFX Also fixed the portal SFX bug

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -30,13 +30,13 @@ public class Portal : MonoBehaviour
         timer.SendMessage("StopLootingTimer");
         //change scene to racing area
         soundManager.PlaySound(soundManager.Sound.TraversePortal);
-        StartCoroutine(waitAndLoadScene());
-    }
-    //we wait 1.1 seconds to let the portal sound play
-    IEnumerator waitAndLoadScene() {
-        yield return /*WaitForSeconds wfs =*/ new WaitForSeconds(float.Parse("1.1"));
+        //StartCoroutine(waitAndLoadScene());
         SceneManager.LoadScene("RacingScene"); //TODO change number 1 for whatever scene number the racing area is
     }
+    //we wait 1.1 seconds to let the portal sound play
+    /*IEnumerator waitAndLoadScene() {
+        yield return new WaitForSeconds(float.Parse("0.05"));
+    }*/
 
     public void enableAndShow()
     {
